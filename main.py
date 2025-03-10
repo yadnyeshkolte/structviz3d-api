496138913d1454de0c4acc738d659bb62c2556ed
Yadnyesh <168337313+yadnyeshkolte@users.noreply.github.com>
2025-03-10 14:16:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://yadnyeshkolte.github.io", 
-        "https://yadnyeshkolte.github.io/structviz3d-react/",
-        "http://localhost:5173",
-        "http://localhost:5174"  
-        ],
+    allow_origins=["*"],  # This allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
